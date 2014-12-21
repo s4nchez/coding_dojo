4330f61153d82b5e2101ab3126b3b595d4a69be8
Ivan Sanchez <s4nchez@gmail.com>
2014-12-21 13:34:29 +0000

    Destructuring

--- katas/RomanNumbersClojure/test/roman/core_test.clj
+++ katas/RomanNumbersClojure/test/roman/core_test.clj
@@ -4,15 +4,13 @@
 
 (defn roman
   ([decimal] (roman decimal '([1000 "M"] [900 "CM"] [500 "D"] [400 "CD"] [100 "C"] [90 "XC"] [50 "L"] [40 "XL"] [10 "X"] [9 "IX"] [5 "V"] [4 "IV"] [1 "I"]) ""))
-  ([decimal roman-numbers result]
-    (let [current-mapping (first roman-numbers)
-          next-roman-numbers (rest roman-numbers)
-          current-roman-decimal (first current-mapping)
-          current-roman (last current-mapping)
-          rest (- decimal current-roman-decimal)]
-      (cond (< rest 0) (roman decimal next-roman-numbers result)
+  ([decimal mappings result]
+    (let [[current-mapping & other-mappings] mappings
+          [current-decimal current-roman] current-mapping
+          rest (- decimal current-decimal)]
+      (cond (< rest 0) (roman decimal other-mappings result)
             (= rest 0) (str result current-roman)
-            :else (roman rest roman-numbers (str result current-roman)))
+            :else (roman rest mappings (str result current-roman)))
       ))
   )
 
