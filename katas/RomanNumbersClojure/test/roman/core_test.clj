281215be13a9355fab233cce55f7e37d9396e353
Ivan Sanchez <s4nchez@gmail.com>
2014-12-21 13:59:36 +0000

    Moved roman function to src

--- katas/RomanNumbersClojure/test/roman/core_test.clj
+++ katas/RomanNumbersClojure/test/roman/core_test.clj
@@ -2,18 +2,6 @@
   (:require [clojure.test :refer :all]
             [roman.core :refer :all]))
 
-(defn roman
-  ([decimal] (roman decimal '([1000 "M"] [900 "CM"] [500 "D"] [400 "CD"] [100 "C"] [90 "XC"] [50 "L"] [40 "XL"] [10 "X"] [9 "IX"] [5 "V"] [4 "IV"] [1 "I"]) ""))
-  ([decimal mappings result]
-    (let [[current-mapping & other-mappings] mappings
-          [current-decimal current-roman] current-mapping
-          rest (- decimal current-decimal)]
-      (cond (< rest 0) (roman decimal other-mappings result)
-            (= rest 0) (str result current-roman)
-            :else (roman rest mappings (str result current-roman)))
-      ))
-  )
-
 (deftest decimal-to-roman
   (testing "key numbers"
     (is (= "I" (roman 1)))
